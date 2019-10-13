9476494f86c73c63262a4cc4ad42b3786456b124
nyklclhst <irfan.aliyafi@outlook.co.id>
2019-10-14 00:35:32 +0700

    update style and script

--- routes/barang.js
+++ routes/barang.js
@@ -53,4 +53,54 @@ router.get('/', function(req,res,next){
     }
 })
 
+router.post('/', function(req,res,next){
+    var Id = Math.floor(Math.random() * 1000).toString();
+    while(Id.length < 5){
+        Id = 0 + Id;
+    }
+    Id = 'ID_' + Id;
+    let cookie = req.cookies.access_token;
+    cookie = cookie.slice(2,66);
+    let uname = req.body.user;
+    let idbar = req.body.idbarang;
+    let jumbar = req.body.jumlah_brg;
+    let kontak = req.body.contact;
+    let kembali = req.body.kembali;
+    let temp = 0;
+    for(i=0;i<ID.length;i++){
+        if(idbar == ID[i]){
+            temp = sum[i];
+        }
+    }
+    if(jumbar > temp){
+        res.render('barang',{id: ID, name: name, sum: sum, msg: ID.length, msg1: 'Jumlah Barang Lebih',msgs: 'user ada'});
+    } else {
+        const data = { "$class": "model.DataPeminjaman",
+        "IDPeminjaman": Id,
+        "NamaPeminjam": uname,
+        "KontakPeminjam": kontak,
+        "JumlahBarang": jumbar,
+        "Barang": "resource:model.DataBarang#"+idbar,
+        "Status": "WAITING",
+        "WaktuPengembalian": kembali
+        };
+        console.log(data)
+        request.post({ url : req.protocol+'://'+ req.hostname+':3000/api/DataPeminjaman',
+            headers: {"X-Access-Token": cookie }, body : data, json : true}, function(error, response, body){
+                console.log('statusCode : '+response.statusCode);
+                if(error === null){
+                    if(response.statusCode === 200){
+                        res.render('barang',{id: ID, name: name, sum: sum, msg: ID.length, msg1: 'Success',msgs: 'user ada'});
+                    } else {
+                        console.log(body)
+                        res.render('barang',{id: ID, name: name, sum: sum, msg: ID.length, msg1: 'Failed',msgs: 'user ada'});
+                    }
+                } else {
+                    console.log(body);
+                    res.render('barang',{id: ID, name: name, sum: sum, msg: ID.length, msg1: 'Failed',msgs: 'user ada'});
+                }
+        })
+    }
+})
+
 module.exports = router;
